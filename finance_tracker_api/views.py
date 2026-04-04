c86e54162825016b252200f97a3ec262c95e6eb4
Prateek <prateekps0123@gmail.com>
2026-04-05 01:30:22 +0530

    Implement Transaction CRUD Views and URL routing

--- finance_tracker_api/views.py
+++ finance_tracker_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Transaction
+from .serializers import TransactionSerializer
 
-# Create your views here.
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Users should only see their own transactions
+        return Transaction.objects.filter(user=self.request.user).order_by('-date')
+
+    def perform_create(self, serializer):
+        # Automatically set the user when creating a transaction
+        serializer.save(user=self.request.user)
