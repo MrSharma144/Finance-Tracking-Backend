02955ab3ddc55bfd3ae035663afffd5096225d77
Prateek <prateekps0123@gmail.com>
2026-04-05 01:21:23 +0530

    Add Serializers for User and Transaction models

---
+++ finance_tracker_api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import CustomUser, Transaction
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
+        read_only_fields = ['id']
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = [
+            'id', 'user', 'amount', 'transaction_type', 
+            'category', 'date', 'description', 
+            'created_at', 'updated_at'
+        ]
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at']
+
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Amount must be greater than zero.")
+        return value
